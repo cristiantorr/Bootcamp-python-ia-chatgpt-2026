f8e253e2b92794773366b2282f86a44717311630
cristiantorr <ps4cristiantorr@gmail.com>
2026-08-28 08:50:38 -0500

    Fix: Remove code

--- clase-01/app.py
+++ clase-01/app.py
@@ -6,11 +6,3 @@ url = "http://localhost:11434/api/generate"
 MODEL = "llama3.2"
 
 
-payload = {
-    "model": MODEL,
-    "prompt": "Explícame qué es Python en una frase.",
-    "stream": False
-}
-
-response = requests.post(url, json=payload) # Aquí enviamos el payload
-print(response.json()["response"])
